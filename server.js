9da1642cfeb34593ad6c382f794ad074d2b68b05
Amit Misra <amit.misra@ishisystems.com>
2017-01-15 07:34:50 -0500

    committing code changes post merge

--- server.js
+++ server.js
@@ -18,4 +18,13 @@ app.use(express.static('public'));
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT);
+=======
+var express = require('express')
+var app = express()
+
+app.use(express.static('public'))
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+>>>>>>> 0a1d5ff5ce29159062efc47ced8a69a4f8414623
 })
