a0ecb142837f2d4b0eab830ebd95dfe1818435b1
Amit Misra <amit.misra@ishisystems.com>
2017-01-15 07:19:22 -0500

    Added start script in packagejson file. Added middleware in server.js for forwarding https requests to http only

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var express = require('express')
-var app = express()
+var express = require('express');
 
-app.use(express.static('public'))
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+var app = express();
+
+app.use(function(req, res, next){
+    if( req.headers["x-forwarded-proto"] === 'http' ) {
+      next();
+    } else {
+      res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT);
 })
