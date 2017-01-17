da70197b32b5c880955005d425c4e35c7498bfd9
Amit Misra <amit.misra@ishisystems.com>
2017-01-17 05:49:14 -0500

    Added bunch of links under about component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,16 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
     <div>
       <h1 className="text-center">About</h1>
       <p>React Weather app created while doing Udemy course by Andrew Mead</p>
+      <ol>
+        <li><Link to="https://github.com/amitmisra16/ReactWeather">Github Repository</Link></li>
+        <li><Link to="http://openweathermap.org/city">OpenweatherMap</Link></li>
+        <li><Link to="https://ancient-bastion-62453.herokuapp.com/">Heroku App</Link></li>
+      </ol>
     </div>
   );
 }
